4926a0cebee84921a60e299cbd7dade151777385
Saad <stcodeslab@gmail.com>
2025-05-17 21:29:50 +0500

    User Model Added

---
+++ MyStore/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyStore.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string? UserName { get; set; }
+    }
+}
